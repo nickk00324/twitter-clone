cbdf58a86aa525cb654d4efbe49df10a09273606
nickk <nickkochornswasdi@gmail.com>
2019-11-12 17:56:16 -0800

    Login functionality, with jwt and passport

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 const db = require('./config/keys').mongoURI;
 
@@ -9,8 +13,13 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('connected to mongodb successfully'))
 .catch( err => console.log(err));
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json());
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 
 
